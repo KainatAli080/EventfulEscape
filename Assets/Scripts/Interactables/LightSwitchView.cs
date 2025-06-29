24d5d04b1fa6ed30ff3ba08e237460073fbd0002
Kainat Ali <kainatali080@gmail.com>
2025-06-29 18:21:28 +0500

    Light Switch Delegate Created
    Created signature, then an instance of that signature inside LightSwitchView. Then added the function call to the instance before invoking in Interact.

--- Assets/Scripts/Interactables/LightSwitchView.cs
+++ Assets/Scripts/Interactables/LightSwitchView.cs
@@ -1,17 +1,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// IInteractable interface means that whoever is implementing this interface MUST
+// have a function of Interact (the abstract function in my IInteractable interface)
 public class LightSwitchView : MonoBehaviour, IInteractable
 {
     [SerializeField] private List<Light> lightsources = new List<Light>();
     private SwitchState currentState;
 
+    public delegate void LightSwitchDelegate(); // The signature of my delegate
+    public LightSwitchDelegate lightSwitch; // An instance of my signature
+
     private void Start() => currentState = SwitchState.Off;
+
+    private void OnEnable()
+    {
+        // Always better to assign instances in onEnable
+        lightSwitch = onLightSwitchToggled;
+    }
 
     public void Interact()
     {
-        //Todo - Implement Interaction
+        // Now to invoke our delegate
+        lightSwitch.Invoke();
     }
+
+    private void onLightSwitchToggled()
+    {
+        toggleLights();
+        GameService.Instance.GetInstructionView().HideInstruction();
+        GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.SwitchSound);
+    }
+
     private void toggleLights()
     {
         bool lights = false;
